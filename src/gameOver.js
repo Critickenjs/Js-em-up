98a04cf5efb73ae94c921668c7a9c1ec2f3c121f
yliess El Atifi <yliess.elatifi.etu@univ-lille.fr>
2024-02-26 17:52:30 +0100

    Ajout de la classe gameOver

---
+++ src/gameOver.js
@@ -0,0 +1,22 @@
+import { Player } from './player.js';
+export default class GameOver {
+	constructor(player) {
+		this.player = player;
+		this.scoreElement = document.querySelector('#scoreValue');
+		this.restartButton = document.querySelector('#restartButton');
+		this.restartButton.addEventListener('click', this.restartGame.bind(this));
+		this.element = document.querySelector('.gameOver');
+	}
+	show() {
+		this.element.style.display = 'flex';
+	}
+
+	hide() {
+		this.element.style.display = 'none';
+	}
+
+	restartGame() {
+		this.hide();
+		this.player.restart();
+	}
+}
