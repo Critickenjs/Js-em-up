f86659acd403a0296c723a9a4b976180d61a59b2
Camille Fourmaintraux <camille.fourmaintraux.etu@univ-lille.fr>
2024-02-28 17:21:56 +0000

    Fonction getRandomInt amélioré pour prendre un minimun

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
 export function getRandomInt(max) {
-	//Return random int between 0 and max
+	//Return random int between 0 included and max included
 	return Math.floor(Math.random() * max);
+}
+
+export function getRandomIntWithMin(min, max) {
+	//Return random int between min included and max included
+	return Math.floor((Math.random() * (max-min+1))+min);
 }
