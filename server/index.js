20f2db32193fe15f8f500d8ef00c3475e061784e
Yliess El atifi <yliess.elatifi.etu@univ-lille.fr>
2024-03-14 12:23:35 +0100

    Add wildcard route to serve index.html file

--- server/index.js
+++ server/index.js
@@ -9,6 +9,10 @@ app.use(express.static('client'));
 
 console.log(`start`);
 
+app.get('*', (req, res) => {
+	res.send('' + fs.readFileSync('client/index.html'));
+});
+
 const port = process.env.PORT || 8000;
 httpServer.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}/`);
