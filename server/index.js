335bd864a17c6fa737cd1b16083b6d22f5cc006b
Yliess El atifi <yliess.elatifi.etu@univ-lille.fr>
2024-03-14 12:31:35 +0100

    Update webpack configuration and add webpack middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import http from 'http';
 import fs from 'fs';
 import express from 'express';
+import addWebpackMiddleware from './middlewares/addWebpackMiddleware.js';
 
 const app = express();
+addWebpackMiddleware(app);
 const httpServer = http.createServer(app);
 
 app.use(express.static('client'));
