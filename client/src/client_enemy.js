6826d60a14259e05e8dcd4d41882a19b93d4bf3b
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-03-17 19:41:31 +0100

    Apparence changeante en fonction de l'ennemi

--- client/src/client_enemy.js
+++ client/src/client_enemy.js
@@ -11,6 +11,25 @@ export default class Client_Enemy extends Client_Entity {
 		this.lifes=lifes;
 		this.image = new Image();
 		this.image.src = './public/res/images/enemy.png';
+		switch (this.type) {
+			case 'red':
+				this.width=Client_Enemy.width;
+				this.height=Client_Enemy.height
+				break;
+			case 'purple':
+				this.width=Client_Enemy.width;
+				this.height=Client_Enemy.height;
+				break;
+			case 'orange':
+				this.height = Client_Enemy.height * 1.5 | 0;
+				this.width = Client_Enemy.width * 1.5 | 0;
+				break;
+			case 'darkred':
+				this.height = (Client_Enemy.height * (this.lifes / 1.3)) | 0;
+				this.width = (Client_Enemy.width * (this.lifes / 1.3)) | 0;
+				this.image.src = './public/res/images/asteroid'+this.lifes+'.png';
+				break;
+		}
 	}
 
 	render(context) {
