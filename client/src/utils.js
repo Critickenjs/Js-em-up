6f64797c818b1e3c5933d07302705f9097228b73
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-03-24 17:53:15 +0100

    Ajout d'une fonction coinFlip permettant de retourner un boolean au hasard

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,3 +8,12 @@ export function getRandomIntWithMin(min, max) {
 	//Return random int between min included and max included
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export function coinFlip() {
+	console.log(getRandomInt(2));
+	if (getRandomInt(2)==0){
+		return true;
+	}else{
+		return false;
+	}
+}
