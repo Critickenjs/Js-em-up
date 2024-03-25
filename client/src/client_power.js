6089e8fafe5d453eb0f32f0ae0e923e4ddeec3dc
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-03-25 23:38:12 +0100

    Réajout des images pour les power-up

--- client/src/client_power.js
+++ client/src/client_power.js
@@ -9,12 +9,62 @@ export default class Client_Power extends Client_Entity {
 	constructor(posX, posY, type) {
 		super(posX,posY,Client_Power.width,Client_Power.height);
 		this.type=type;
+		this.imageBonusShield = new Image();
+		this.imageBonusShield.src = './public/res/images/bonusShield.png';
+		this.imageBonusLife = new Image();
+		this.imageBonusLife.src = './public/res/images/bonusLife.png';
+		this.imageBonusArrows = new Image();
+		this.imageBonusArrows.src = './public/res/images/bonusArrows.png';
+		this.imageIce = new Image();
+		this.imageIce.src = './public/res/images/ice.png';
+		
 	}
 
 	render(context) {
 		super.render(context);
 		context.beginPath();
-        context.fillStyle = 'yellow';
-		context.fillRect(this.posX, this.posY, this.width, this.height);
+        switch(this.type){
+			case('invincible'):
+				context.drawImage(
+					this.imageBonusShield,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			case('life'):
+				context.drawImage(
+					this.imageBonusLife,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			case('ScoreMultiplierBonus'):
+				context.drawImage(
+					this.imageBonusArrows,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			case('ice'):
+					context.drawImage(
+					this.imageIce,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			default:
+				context.strokeStyle='yellow';
+				context.rect(this.posX,this.posY,this.width,this.height);
+				context.stroke();
+			break;
+		}
 	}
 }
