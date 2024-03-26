8f0efaddaa6b2a03683e9bbaa865e763b998bab3
Yliess El atifi <yliess.elatifi.etu@univ-lille.fr>
2024-03-26 14:37:21 +0100

    Add update method to Scoreboard class

--- client/src/scoreboard.js
+++ client/src/scoreboard.js
@@ -10,4 +10,12 @@ export default class Scoreboard extends View {
 	hide() {
 		this.element.style.display = 'none';
 	}
+	update(data) {
+		this.element.querySelector('#scoreList').innerHTML = '';
+		for (const playerName in data) {
+			const li = document.createElement('li');
+			li.textContent = `${playerName} : ${data[playerName]}`;
+			this.element.querySelector('#scoreList').appendChild(li);
+		}
+	}
 }
