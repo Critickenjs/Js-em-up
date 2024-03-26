4a34ed6752346b5bc2b424dfac0da11b7abc8be2
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-03-26 11:51:06 +0100

    Implémentation des nouvelles images dans le code

--- client/src/client_power.js
+++ client/src/client_power.js
@@ -17,6 +17,10 @@ export default class Client_Power extends Client_Entity {
 		this.imageBonusArrows.src = './public/res/images/bonusArrows.png';
 		this.imageIce = new Image();
 		this.imageIce.src = './public/res/images/ice.png';
+		this.imageBonusLaser = new Image();
+		this.imageBonusLaser.src = './public/res/images/bonusLaser.png';
+		this.imageBonusPerforation = new Image();
+		this.imageBonusPerforation.src = './public/res/images/bonusPerforation.png';
 		
 	}
 
@@ -54,6 +58,24 @@ export default class Client_Power extends Client_Entity {
 			case('ice'):
 					context.drawImage(
 					this.imageIce,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			case('laser'):
+				context.drawImage(
+					this.imageBonusLaser,
+					this.posX,
+					this.posY,
+					this.width,
+					this.height
+				);
+			break;
+			case('perforation'):
+					context.drawImage(
+					this.imageBonusPerforation,
 					this.posX,
 					this.posY,
 					this.width,
