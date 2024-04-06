f089c70b3a118ef107d9098452cb0c9d70ecde1f
Camille Fourmaintraux <123071999+CamilleFourmaintraux@users.noreply.github.com>
2024-04-06 20:58:30 +0200

    Tests de WavesManager à 100%

--- server/test/wavesManager.test.js
+++ server/test/wavesManager.test.js
@@ -1,6 +1,9 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import WavesManager from '../wavesManager.js';
+import Game from '../game.js';
+import Enemy from '../enemy.js';
+import Player from '../player.js';
 
 describe('WavesManager', () => {
     describe('firstWave', () => {
@@ -27,24 +30,26 @@ describe('WavesManager', () => {
         });
     });
 
-    describe('nextWave', () => {
-        it('should update properties correctly', () => {
-            const wavesManager = new WavesManager();
-            wavesManager.nextWave(1);
-
-            assert.strictEqual(wavesManager.hasABoss, false);
-            assert.strictEqual(wavesManager.waveNumber, 2);
-            assert.strictEqual(wavesManager.waveNumberOfEnemysSpawned, 0);
-            assert.strictEqual(wavesManager.waveMaxNumberOfEnemys, 5);
+    describe('interactions', () => {
+        it('should update player and ennemies interactions correctly', () => {
+            const game = new Game(1);
+            game.players.set('player', new Player(100, 100, "Willy"));
+            game.wavesManager.enemys.push(new Enemy(105, 105, 1));
+            game.wavesManager.waveMaxNumberOfEnemys = 5;
+            game.wavesManager.waveNumberOfEnemysSpawned = 5;
+            game.players.get('player').invincible = false;
+            assert.strictEqual(game.gameData.teamLifes, 3);
+            assert.strictEqual(game.players.get('player').alive, true);
+            assert.strictEqual(game.players.get('player').invincible, false);
+            assert.strictEqual(game.wavesManager.enemys[0].isDead, false);
+            game.wavesManager.updateEnnemiesToPlayersInteractions(game);
+            assert.strictEqual(game.gameData.teamLifes, 2);
+            assert.strictEqual(game.players.get('player').alive, false);
+            assert.strictEqual(game.wavesManager.enemys[0].isDead, true);
         });
     });
 
 
-
-
-
-});
-describe('WavesManager', () => {
     describe('firstWave', () => {
         it('should initialize properties correctly', () => {
             const wavesManager = new WavesManager();
@@ -70,11 +75,12 @@ describe('WavesManager', () => {
     describe('nextWave', () => {
         it('should update properties correctly', () => {
             const wavesManager = new WavesManager();
+            wavesManager.enemys.push(new Enemy(100, 101, 1));
             wavesManager.nextWave(1);
 
             assert.strictEqual(wavesManager.hasABoss, false);
             assert.strictEqual(wavesManager.waveNumber, 2);
-            assert.strictEqual(wavesManager.waveNumberOfEnemysSpawned, 0);
+            assert.strictEqual(wavesManager.waveNumberOfEnemysSpawned, 1);
             assert.strictEqual(wavesManager.waveMaxNumberOfEnemys, 5);
         });
 
@@ -120,4 +126,6 @@ describe('WavesManager', () => {
             assert.strictEqual(result, true);
         });
     });
+
+
 });
